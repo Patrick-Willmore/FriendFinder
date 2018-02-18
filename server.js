8695479f65a5b58119e273ceb0f800c40db66b5b
Patrick Willmore <patman1127@gmail.com>
2018-02-18 11:53:41 -0700

    second commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+const port = process.env.PORT || 3000
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, function(){
+    console.log("App listening on PORT " + port);
+});
