b70b60f4b34bd1b895f4245e2819e22c6ba2c97a
Patrick Willmore <patman1127@gmail.com>
2018-02-18 19:11:27 -0700

    commit.1

--- routing/htmlroutes.js
+++ routing/htmlroutes.js
@@ -8,10 +8,9 @@ module.exports = function(app) {
 
 
 
-	app.get("*", function(req, res) {
+	app.get('*', function(req, res){
 		res.sendFile(path.join(__dirname, "/../public/home.html"));
 	  });
-
 
 
 };
